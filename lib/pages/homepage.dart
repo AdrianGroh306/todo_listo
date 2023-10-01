4daf927455c0c8cbf3571d35669157d05ca925c6
AdrianGroh <116496298+AdrianGroh306@users.noreply.github.com>
2023-10-01 16:14:39 +0200

    cleaning up same naming and code

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,236 +1,227 @@
+// Dart imports
 import 'dart:async';
-import 'package:circle_progress_bar/circle_progress_bar.dart';
+
+// Package imports
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+
+// Local imports
 import 'package:todo/util/addTodo_box.dart';
 import 'package:todo/util/sideMenu_bar.dart';
 import 'package:todo/util/myTodoTile.dart';
 import '../util/MenuItem.dart';
+import 'package:circle_progress_bar/circle_progress_bar.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _controller = TextEditingController();
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  List<Map<String, dynamic>> toDoList = [];
-  List<ValueNotifier<bool>> taskCompletionList = [];
-  String? selectedList;
+  final TextEditingController _todoController = TextEditingController();
+  final FirebaseFirestore _firestoreDB = FirebaseFirestore.instance;
+
+  List<Map<String, dynamic>> _todos = [];
+  List<ValueNotifier<bool>> _taskCompletionNotifiers = [];
+  String? _selectedTaskListId;
 
   @override
   void initState() {
     super.initState();
-    fetchTodosForSelectedList();
+    _fetchTodos();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void fetchTodosForSelectedList() async {
+  // Fetch tasks associated with the selected list
+  void _fetchTodos() async {
     try {
       final userId = FirebaseAuth.instance.currentUser?.uid;
-      if (userId != null) {
-        final querySnapshot = await _firestore
+      if (userId != null && _selectedTaskListId != null) {
+        final querySnapshot = await _firestoreDB
             .collection('todos')
             .where('userId', isEqualTo: userId)
-            .where('listId', isEqualTo: selectedList)
+            .where('listId', isEqualTo: _selectedTaskListId)
             .get();
 
         setState(() {
-          toDoList = querySnapshot.docs.map((doc) {
-            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-            data['documentId'] = doc.id;
-            return data;
+          _todos = querySnapshot.docs.map((doc) {
+            return {
+              ...doc.data(),
+              'documentId': doc.id,
+            };
           }).toList();
 
-          taskCompletionList = toDoList.map((task) {
+          _taskCompletionNotifiers = _todos.map((task) {
             return ValueNotifier<bool>(task['taskCompleted'] ?? false);
           }).toList();
         });
       }
     } catch (e) {
-      print('Error fetching Todos for the selected list: $e');
+      print('[Error] Fetching Todos for the selected list: $e');
     }
   }
 
-  void saveNewTask(String? taskName) async {
+  // Save a new task to Firestore
+  void _saveTodos(String? taskName) async {
     try {
-      String userId = FirebaseAuth.instance.currentUser!.uid;
+      final userId = FirebaseAuth.instance.currentUser!.uid;
 
-      if (selectedList != null) {
-        DocumentReference docRef = await _firestore.collection('todos').add({
+      if (_selectedTaskListId != null && taskName != null) {
+        DocumentReference docRef = await _firestoreDB.collection('todos').add({
           'userId': userId,
-          'listId': selectedList,
+          'listId': _selectedTaskListId,
           'taskName': taskName,
           'taskCompleted': false,
         });
 
-        String documentId = docRef.id;
-
         setState(() {
-          toDoList.add({
-            'documentId': documentId,
+          _todos.add({
+            'documentId': docRef.id,
             'taskName': taskName,
             'taskCompleted': false,
           });
-          taskCompletionList.add(ValueNotifier<bool>(false));
-          _controller.clear();
+          _taskCompletionNotifiers.add(ValueNotifier<bool>(false));
+          _todoController.clear();
         });
 
         Navigator.of(context).pop();
       }
     } catch (e) {
-      print('Error saving task: $e');
+      print('[Error] Saving task: $e');
     }
   }
 
-  Future<void> updateTaskCompletionStatus(
-      String documentId, bool newCompletionStatus) async {
+  // Update task's name in Firestore
+  void _updateTaskName(String documentId, String newTaskName) async {
     try {
-      await _firestore
-          .collection('todos')
-          .doc(documentId)
-          .update({'taskCompleted': newCompletionStatus});
+      await _firestoreDB.collection('todos').doc(documentId).update({
+        'taskName': newTaskName,
+      });
+
+      setState(() {
+        final index =
+        _todos.indexWhere((task) => task['documentId'] == documentId);
+        if (index != -1) {
+          _todos[index]['taskName'] = newTaskName;
+        }
+      });
     } catch (e) {
-      print('Error updating task completion status: $e');
-      rethrow;
+      print('[Error] Updating task name: $e');
     }
   }
 
-  void checkBoxChanged(bool? value, int index) async {
-    if (index < 0 || index >= toDoList.length) {
-      print('Error updating task: Invalid index.');
+  // Update task's completion status
+  Future<void> _updateTaskCompletionStatus(String documentId, bool newCompletionStatus) async {
+    try {
+      await _firestoreDB.collection('todos').doc(documentId).update({
+        'taskCompleted': newCompletionStatus,
+      });
+    } catch (e) {
+      print('[Error] Updating task completion status: $e');
+    }
+  }
+
+  // Handle check-box change
+  void _checkBoxChanged(bool? value, int index) async {
+    if (index < 0 || index >= _todos.length) {
+      print('[Error] Invalid index while updating task.');
       return;
     }
 
-    final task = toDoList[index];
-
+    final task = _todos[index];
     if (!task.containsKey('documentId')) {
-      print('Error updating task: Document ID not found.');
+      print('[Error] Document ID not found while updating task.');
       return;
     }
 
     final documentId = task['documentId'] as String;
+    final newValue = value ?? false;
+    await _updateTaskCompletionStatus(documentId, newValue);
 
-    try {
-      await _firestore.collection('todos').doc(documentId).update({
-        'taskCompleted': value ?? false,
-        'taskName': task['taskName'],
+    if (mounted) {
+      setState(() {
+        task['taskCompleted'] = newValue;
+        _taskCompletionNotifiers[index].value = newValue;
       });
-
-      if (mounted) {
-        setState(() {
-          task['taskCompleted'] = value ?? false;
-          taskCompletionList[index].value = value ?? false;
-        });
-      }
-    } catch (e) {
-      print('Error updating task: $e');
     }
   }
 
-  void cancelNewTask() {
-    Navigator.of(context).pop();
-    _controller.clear();
-  }
-
-  void createTask() {
+  // Show dialog for creating a task
+  void _createTask() {
     showDialog(
       context: context,
       builder: (context) {
         return DialogBox(
-          controller: _controller,
-          onSave: saveNewTask,
-          onCancel: cancelNewTask,
-          onSubmitted: saveNewTask,
+          controller: _todoController,
+          onSave: _saveTodos,
+          onCancel: () {
+            Navigator.of(context).pop();
+            _todoController.clear();
+          },
+          onSubmitted: _saveTodos,
         );
       },
     );
   }
 
-  void updateTaskName(String documentId, String newTaskName) async {
-    try {
-      await _firestore
-          .collection('todos')
-          .doc(documentId)
-          .update({'taskName': newTaskName});
-
-      setState(() {
-        final index =
-            toDoList.indexWhere((task) => task['documentId'] == documentId);
-        if (index != -1) {
-          toDoList[index]['taskName'] = newTaskName;
-        }
-      });
-    } catch (e) {
-      print('Error updating task name: $e');
-    }
-  }
-
-  void deleteTask(int index) async {
-    Map<String, dynamic> task = toDoList[index];
-
+  // Delete a single task
+  void _deleteTask(int index) async {
+    final task = _todos[index];
     if (!task.containsKey('documentId')) {
-      print('Error deleting task: Document ID not found.');
+      print('[Error] Document ID not found while deleting task.');
       return;
     }
 
-    String documentId = task['documentId'] as String;
-
-    await _firestore.collection('todos').doc(documentId).delete();
+    final documentId = task['documentId'] as String;
+    await _firestoreDB.collection('todos').doc(documentId).delete();
 
     setState(() {
-      toDoList.removeAt(index);
-      taskCompletionList.removeAt(index);
+      _todos.removeAt(index);
+      _taskCompletionNotifiers.removeAt(index);
     });
   }
 
-  void deleteAllTask() async {
+  // Delete all tasks for the selected list
+  void _deleteAllTask() async {
     try {
-      String userId = FirebaseAuth.instance.currentUser!.uid;
+      final userId = FirebaseAuth.instance.currentUser!.uid;
 
-      QuerySnapshot snapshot = await _firestore
+      final snapshot = await _firestoreDB
           .collection('todos')
           .where('userId', isEqualTo: userId)
-          .where('listId', isEqualTo: selectedList)
+          .where('listId', isEqualTo: _selectedTaskListId)
           .get();
 
-      for (QueryDocumentSnapshot doc in snapshot.docs) {
+      for (final doc in snapshot.docs) {
         await doc.reference.delete();
       }
 
       setState(() {
-        toDoList.clear();
-        taskCompletionList.clear();
+        _todos.clear();
+        _taskCompletionNotifiers.clear();
       });
     } catch (e) {
-      print('Error deleting all tasks: $e');
+      print('[Error] Deleting all tasks: $e');
     }
   }
 
-  void signUserOut() {
+  // Logout user
+  void _signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       drawer: SideMenu(
-        selectedListId: selectedList,
+        selectedListId: _selectedTaskListId,
         onSelectedListChanged: (listId) {
           setState(() {
-            selectedList = listId;
-            fetchTodosForSelectedList();
+            _selectedTaskListId = listId;
+            _fetchTodos();
           });
         },
       ),
@@ -264,12 +255,12 @@ class _MyHomePageState extends State<MyHomePage> {
                         foregroundColor:
                             Theme.of(context).colorScheme.secondary,
                         backgroundColor: Theme.of(context).colorScheme.tertiary,
-                        value: toDoList.isEmpty
+                        value: _todos.isEmpty
                             ? 0.0
-                            : toDoList
+                            : _todos
                                     .where((task) => task['taskCompleted'])
                                     .length /
-                                toDoList.length,
+                                _todos.length,
                         animationDuration: const Duration(seconds: 1),
                       ),
                     ),
@@ -288,10 +279,10 @@ class _MyHomePageState extends State<MyHomePage> {
             PopupMenuButton<MenuItem>(
               onSelected: (value) {
                 if (value == MenuItem.item1) {
-                  deleteAllTask();
+                  _deleteAllTask();
                 }
                 if (value == MenuItem.item2) {
-                  signUserOut();
+                  _signUserOut();
                 }
                 if (value == MenuItem.item3) {}
               },
@@ -368,7 +359,7 @@ class _MyHomePageState extends State<MyHomePage> {
         width: 140,
         child: FloatingActionButton.extended(
           elevation: 0,
-          onPressed: createTask,
+          onPressed: _createTask,
           backgroundColor: Theme.of(context).colorScheme.primary,
           shape: StadiumBorder(
               side: BorderSide(
@@ -387,21 +378,21 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         color: Theme.of(context).colorScheme.background,
         child: ListView.builder(
-          itemCount: toDoList.length,
+          itemCount: _todos.length,
           itemBuilder: (context, index) {
-            final task = toDoList[index];
+            final task = _todos[index];
 
             return ValueListenableBuilder<bool>(
-              valueListenable: taskCompletionList[index],
+              valueListenable: _taskCompletionNotifiers[index],
               builder: (context, value, _) {
                 return ToDoTile(
                   key: ValueKey(task['documentId']),
                   taskName: task['taskName'] as String,
                   taskCompleted: value,
-                  onChanged: (newValue) => checkBoxChanged(newValue, index),
-                  deleteFunction: (context) => deleteTask(index),
+                  onChanged: (newValue) => _checkBoxChanged(newValue, index),
+                  deleteFunction: (context) => _deleteTask(index),
                   onTaskNameChanged: (newTaskName) =>
-                      updateTaskName(task['documentId'], newTaskName),
+                      _updateTaskName(task['documentId'], newTaskName),
                 );
               },
             );
